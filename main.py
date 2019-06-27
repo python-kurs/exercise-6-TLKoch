b35dea39d0dc4cf36ceeb6779f007694a4334430
TLKoch <Tiziana.Li@t-online.de>
2019-06-27 13:33:56 +0200

    solved

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ from pathlib import Path
 input_dir  = Path("data")
 
 # 1. Read the Scene that you downloaded from the data directory using SatPy. [2P]
+from satpy import Scene, find_files_and_readers, writers, composites
+
+files = find_files_and_readers(base_dir="C:/Users/Kochti/Desktop/Studium/Python/exercise-6-TLKoch/data/", reader="seviri_l1b_nc")
+scn = Scene(filenames=files)
+
 
 # 2. Load the composites "natural_color" and "convection" [2P]
+scn.load(["natural_color"])
+scn.load(["convection"])
 
 # 3. Resample the fulldisk to the Dem. Rep. Kongo and its neighbours [4P] 
 #    by defining your own area in Lambert Azimuthal Equal Area. 
@@ -15,4 +22,30 @@ input_dir  = Path("data")
 #      - projection x/y coordinates of lower left: -15E5
 #      - projection x/y coordinates of upper right: 15E5 
 
+
+from pyresample.geometry import AreaDefinition
+
+area_id = "Dem. Rep. Kongo"
+description = "Dem. Rep. Kongo and its adjacent areas in Lambert Azimuthal Equal Area projection"
+proj_id = "Kongo"
+proj_dict = {"proj": "laea", "lat_0": -3, "lon_0": 23}
+width = 500 
+height = 500
+llx =  -15E5
+lly =  -15E5
+urx =  15E5
+ury =  15E5
+area_extent = (llx, lly, urx, ury)
+area_def = AreaDefinition(area_id, proj_id, description, proj_dict, width, height, area_extent)
+local_scn = scn.resample(area_def)
+
+
+
+
 # 4. Save both loaded composites of the resampled Scene as simple png images. [2P]
+
+local_scn.save_datasets(writer="simple_image",
+                  datasets=["natural_color","convection"],
+                  filename="{name}_{start_time:%Y%m%d_%H%M%S}.png",
+                  base_dir="C:/Users/Kochti/Desktop/Studium/Python/exercise-6-TLKoch/solution")
+
